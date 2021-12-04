9d0ab523f319541380f6894ac4032510c551127b
hoo <hoo0681@gachon.ac.kr>
2021-12-04 12:46:13 +0900

    sync test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import tensorflow as tf 
 import torch
-
+print('asdf')
 print(tf.reduce_sum(tf.random.normal([1000, 1000])))
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
