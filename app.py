01f22ef5fd5318430f2374166a54cda951ea5223
hoo <hoo0681@gachon.ac.kr>
2021-12-04 16:02:22 +0900

    time

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import tensorflow as tf 
-import torch
-print('asdf')
-print(tf.reduce_sum(tf.random.normal([1000, 1000])))
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
+#import tensorflow as tf 
+#import torch
+#print('asdf')
+#print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#print(device)
+import time
+print ("App started")
+while True:
+    time.sleep(1)
