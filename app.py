ffc0e326ccfbfa584e7bd4a3b0988b1420b89b31
hoo <hoo0681@gachon.ac.kr>
2021-12-04 17:57:33 +0900

    FLOWER test

--- app.py
+++ app.py
@@ -4,7 +4,11 @@
 #print(tf.reduce_sum(tf.random.normal([1000, 1000])))
 #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #print(device)
-import time
-print ("App started")
-while True:
-    time.sleep(1)
+#import time
+#print ("App started")
+#while True:
+#    time.sleep(1)
+import flwr as fl
+
+# Start Flower server for three rounds of federated learning
+fl.server.start_server(config={"num_rounds": 3})
